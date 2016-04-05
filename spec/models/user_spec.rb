f442d10a722d2b5c8a4969c8c6fc34a06f9609f0
Eric Darsey <letsgomavs22@yahoo.com>
2016-04-05 11:35:34 -0400

    fleshed out CRUD tests for Posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 feature "As an user, I want to add an item, So that others can review it" do
+  let title_one = Post.create(title: "EastWillyB", media_type: "Video", description: "Willie Jr.’s got his work cut out for him. Running Bushwick’s favorite Latino bar isn’t easy when the old timers retire, the new generation forgets their culture, and the hipsters start taking over the hood. As if it couldn’t get worse, Maggie, Willie Jr.’s ex-fiance, has taken up with Willie Jr.’s arch-nemesis, Albert, and is skillfully transforming Albert’s competing neighborhood bar into a moneymaker, as they cater to the whims of the new residents. Along with the help of his bar regulars, Willie Jr. tries to come up with a solution quickly, or risk losing the two loves of his life: Maggie and his sports bar.", link: "http://www.eastwillyb.com/")
+
   scenario "User adds item successfully" do
     visit '/'
     click_button('Post a Series')
-    fill_in('Title') with: "EastWillyB"
-    fill_in('Media Type') with: "Video"
-    fill_in('Description') with: "Willie Jr.’s got his work cut out for him. Running Bushwick’s favorite Latino bar isn’t easy when the old timers retire, the new generation forgets their culture, and the hipsters start taking over the hood. As if it couldn’t get worse, Maggie, Willie Jr.’s ex-fiance, has taken up with Willie Jr.’s arch-nemesis, Albert, and is skillfully transforming Albert’s competing neighborhood bar into a moneymaker, as they cater to the whims of the new residents. Along with the help of his bar regulars, Willie Jr. tries to come up with a solution quickly, or risk losing the two loves of his life: Maggie and his sports bar."
-    fill_in('Link') with: "http://www.eastwillyb.com/"
+    fill_in('Title') with: title_one.title
+    fill_in('Media Type') with: title_one.media_type
+    fill_in('Description') with: title_one.description
+    fill_in('Link') with: title_one.link
     expect(page).to have_content('EastWillyB')
     expect(page).to have_content('Willie Jr.’s got his work cut out for him. Running Bushwick’s favorite Latino bar isn’t easy when the old timers retire, the new generation forgets their culture, and the hipsters start taking over the hood. As if it couldn’t get worse, Maggie, Willie Jr.’s ex-fiance, has taken up with Willie Jr.’s arch-nemesis, Albert, and is skillfully transforming Albert’s competing neighborhood bar into a moneymaker, as they cater to the whims of the new residents. Along with the help of his bar regulars, Willie Jr. tries to come up with a solution quickly, or risk losing the two loves of his life: Maggie and his sports bar.')
   end
@@ -15,9 +17,9 @@ feature "As an user, I want to add an item, So that others can review it" do
   scenario "User adds item unsuccessfully" do
     visit '/'
     click_button('Post a Series')
-    fill_in('Title') with: "EastWillyB"
-    fill_in('Media Type') with: "Video"
-    fill_in('Description') with: "Willie Jr.’s got his work cut out for him. Running Bushwick’s favorite Latino bar isn’t easy when the old timers retire, the new generation forgets their culture, and the hipsters start taking over the hood. As if it couldn’t get worse, Maggie, Willie Jr.’s ex-fiance, has taken up with Willie Jr.’s arch-nemesis, Albert, and is skillfully transforming Albert’s competing neighborhood bar into a moneymaker, as they cater to the whims of the new residents. Along with the help of his bar regulars, Willie Jr. tries to come up with a solution quickly, or risk losing the two loves of his life: Maggie and his sports bar."
+    fill_in('Title') with: title_one.title
+    fill_in('Media Type') with: title_one.media_type
+    fill_in('Description') with: title_one.description
 
     expect(page).to_not have('EastWillyB')
     expect(page).to_not have('Willie Jr.’s got his work cut out for him. Running Bushwick’s favorite Latino bar isn’t easy when the old timers retire, the new generation forgets their culture, and the hipsters start taking over the hood. As if it couldn’t get worse, Maggie, Willie Jr.’s ex-fiance, has taken up with Willie Jr.’s arch-nemesis, Albert, and is skillfully transforming Albert’s competing neighborhood bar into a moneymaker, as they cater to the whims of the new residents. Along with the help of his bar regulars, Willie Jr. tries to come up with a solution quickly, or risk losing the two loves of his life: Maggie and his sports bar.')
@@ -26,73 +28,81 @@ feature "As an user, I want to add an item, So that others can review it" do
   scenario "User adds item unsuccessfully" do
     visit '/'
     click_button('Post a Series')
-    fill_in('Media Type') with: "Video"
-    fill_in('Description') with: "Willie Jr.’s got his work cut out for him. Running Bushwick’s favorite Latino bar isn’t easy when the old timers retire, the new generation forgets their culture, and the hipsters start taking over the hood. As if it couldn’t get worse, Maggie, Willie Jr.’s ex-fiance, has taken up with Willie Jr.’s arch-nemesis, Albert, and is skillfully transforming Albert’s competing neighborhood bar into a moneymaker, as they cater to the whims of the new residents. Along with the help of his bar regulars, Willie Jr. tries to come up with a solution quickly, or risk losing the two loves of his life: Maggie and his sports bar."
-    fill_in('Link') with: "http://www.eastwillyb.com/"
+    fill_in('Media Type') with: title_one.media_type
+    fill_in('Description') with: title_one.description
+    fill_in('Link') with: title_one.link
 
     expect(page).to_not have('http://www.eastwillyb.com/')
     expect(page).to_not have('Willie Jr.’s got his work cut out for him. Running Bushwick’s favorite Latino bar isn’t easy when the old timers retire, the new generation forgets their culture, and the hipsters start taking over the hood. As if it couldn’t get worse, Maggie, Willie Jr.’s ex-fiance, has taken up with Willie Jr.’s arch-nemesis, Albert, and is skillfully transforming Albert’s competing neighborhood bar into a moneymaker, as they cater to the whims of the new residents. Along with the help of his bar regulars, Willie Jr. tries to come up with a solution quickly, or risk losing the two loves of his life: Maggie and his sports bar.')
   end
 end
 
-feature "As an user, I want to view a list of items, So that I can pick items to review" do
+feature "As a user, I want to view a list of items, So that I can pick items to review" do
+  let title_one = Post.create(title: "EastWillyB", media_type: "Video", description: "Willie Jr.’s got his work cut out for him. Running Bushwick’s favorite Latino bar isn’t easy when the old timers retire, the new generation forgets their culture, and the hipsters start taking over the hood. As if it couldn’t get worse, Maggie, Willie Jr.’s ex-fiance, has taken up with Willie Jr.’s arch-nemesis, Albert, and is skillfully transforming Albert’s competing neighborhood bar into a moneymaker, as they cater to the whims of the new residents. Along with the help of his bar regulars, Willie Jr. tries to come up with a solution quickly, or risk losing the two loves of his life: Maggie and his sports bar.", link: "http://www.eastwillyb.com/")
+  let title_two = Post.create(title: "Ylse: A Web Series", media_type: "Video", description: "The Funniest TV show not on television", link: "http://www.ylse.net")
+  let title_three = Post.create(title: "Valley Meadows", media_type: "Video", description: "Two dudes try to rap", link: "https://www.youtube.com/watch?v=q5R2Iy-5EUA")
   scenario "user sees list of items" do
-    #add factorygirl items with title and link
-    #add factorygirl items with title and link
-    #add factorygirl items with title and link
     visit '/'
-    expect(page).to have_content(#1st title)
-    expect(page).to have_content(#2nd title)
-    expect(page).to have_content(#3rd title)
+    expect(page).to have_content("EastWillyB")
+    expect(page).to have_content("Ylse: A Web Series")
+    expect(page).to have_content("Valley Meadows")
   end
 
   scenario "user sees detailed list of an item" do
-    #add factorygirl items with title and link
-    #add factorygirl items with title and link
-    #add factorygirl items with title and link
     visit '/'
-    click_link('#1st title')
-    expect(page).to have_content(#1st title)
-    expect(page).to have_content(#1st title link)
+    click_link("Valley Meadows")
+    expect(page).to have_content("Valley Meadows")
+    expect(page).to have_content("Video")
+    expect(page).to have_content("Two dudes try to rap")
+    expect(page).to have_content("https://www.youtube.com/watch?v=q5R2Iy-5EUA")
   end
 end
 
 feature "As a user, I want to update an item's information, So that I can correct errors or provide new information" do
+
+  let title_one = Post.create(title: "EastWillyB", media_type: "Video", description: "Willie Jr.’s got his work cut out for him. Running Bushwick’s favorite Latino bar isn’t easy when the old timers retire, the new generation forgets their culture, and the hipsters start taking over the hood. As if it couldn’t get worse, Maggie, Willie Jr.’s ex-fiance, has taken up with Willie Jr.’s arch-nemesis, Albert, and is skillfully transforming Albert’s competing neighborhood bar into a moneymaker, as they cater to the whims of the new residents. Along with the help of his bar regulars, Willie Jr. tries to come up with a solution quickly, or risk losing the two loves of his life: Maggie and his sports bar.", link: "http://www.eastwillyb.com/")
+  let title_two = Post.create(title: "Ylse: A Web Series", media_type: "Video", description: "The Funniest TV show not on television", link: "http://www.ylse.net")
+  let title_three = Post.create(title: "Valley Meadows", media_type: "Video", description: "Two dudes try to rap", link: "https://www.youtube.com/watch?v=q5R2Iy-5EUA")
+
   scenario "User successfully updates the link to the content" do
     visit '/'
-    click_link('#1st title')
+    click_link('Ylse: A Web Series')
     click_button('Update')
-    fill_in('Link') with: "http://www.ylse.net"
+    fill_in('Link') with: "www.imdb.com/title/tt2261139"
     click_button('Save & Update')
-    expect(page).to have_content("http://www.ylse.net")
+    expect(page).to have_content("www.imdb.com/title/tt2261139/")
   end
 
   scenario "User successfully updates the title to the content" do
     visit '/'
-    click_link('#1st title')
+    click_link('Valley Meadows')
     click_button('Update')
-    fill_in('Title') with: "Ylse: A Web Series"
+    fill_in('Title') with: "Valley Meadows Web Show"
     click_button('Save & Update')
-    expect(page).to have_content("Ylse: A Web Series")
+    expect(page).to have_content("Valley Meadows Web Show")
   end
 
   scenario "User successfully updates the description to the content" do
     visit '/'
-    click_link('#1st title')
+    click_link('EastWillyB')
     click_button('Update')
-    fill_in('Description') with: "The Funniest TV show not on television"
+    fill_in('Description') with: "Culture clash with a new generation, on the Eastside"
     click_button('Save & Update')
-    expect(page).to have_content("The Funniest TV show not on television")
+    expect(page).to have_content("Culture clash with a new generation, on the Eastside")
   end
 end
 
 feature "As a user, I want to delete an item, So that no one can review it" do
+  let title_one = Post.create(title: "EastWillyB", media_type: "Video", description: "Willie Jr.’s got his work cut out for him. Running Bushwick’s favorite Latino bar isn’t easy when the old timers retire, the new generation forgets their culture, and the hipsters start taking over the hood. As if it couldn’t get worse, Maggie, Willie Jr.’s ex-fiance, has taken up with Willie Jr.’s arch-nemesis, Albert, and is skillfully transforming Albert’s competing neighborhood bar into a moneymaker, as they cater to the whims of the new residents. Along with the help of his bar regulars, Willie Jr. tries to come up with a solution quickly, or risk losing the two loves of his life: Maggie and his sports bar.", link: "http://www.eastwillyb.com/")
+  let title_two = Post.create(title: "Ylse: A Web Series", media_type: "Video", description: "The Funniest TV show not on television", link: "http://www.ylse.net")
+  let title_three = Post.create(title: "Valley Meadows", media_type: "Video", description: "Two dudes try to rap", link: "https://www.youtube.com/watch?v=q5R2Iy-5EUA")
+
   scenario "User is the creator of the artwork and successfully deletes the artwork" do
     visit '/'
-    click_link('#1st title')
+    click_link('Ylse: A Web Series')
     click_button('Delete')
 
-    expect(page).to_not have_content('#1st title')
+    expect(page).to_not have_content('Ylse: A Web Series')
 
 
 
