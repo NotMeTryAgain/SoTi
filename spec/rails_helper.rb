0dd8bbf44b5760c852dc2c8a74f105d942c20bf1
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-04-06 10:15:58 -0400

    removed unused code

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,6 @@ require 'rspec/rails'
 require 'devise'
 require 'factory_girl_rails'
 require_relative 'support/factory_girl'
-# require_relative 'support/request_helpers.rb'
 
 RSpec.configure do |config|
   config.include Warden::Test::Helpers
