72fdc1b88a06a1add88f5a9a27279960bb2528d5
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-04-06 14:18:43 -0400

    deleted commented out code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    # add avatar later
     devise_parameter_sanitizer.for(:sign_up) << [:username]
     devise_parameter_sanitizer.for(:account_update) << :username
   end
