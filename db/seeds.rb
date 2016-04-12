e6fa7c8cc62bab1d21e72e49354f25815cc44c02
Michael Hebert <michaelhebert@Michaels-MacBook-Pro.local>
2016-04-12 13:18:51 -0400

    appeased the hound, deleted some comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 5.times do
   FactoryGirl.create(:user)
 end
@@ -18,7 +10,7 @@ FactoryGirl.create(:user, username: "pablo-admin", role: "admin")
 end
 
 Post.all.each do |post|
-  20.times do 
+  20.times do
     FactoryGirl.create(:review, post: post)
   end
 end
