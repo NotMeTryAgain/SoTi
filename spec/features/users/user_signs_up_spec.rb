e7710aeb16b40146e717b7d756ea60fba4235f62
Candace Hazlett <candace.hazlett@gmail.com>
2016-04-13 14:17:54 -0400

    third attempt on hound errors

--- spec/features/users/user_signs_up_spec.rb
+++ spec/features/users/user_signs_up_spec.rb
@@ -24,15 +24,15 @@ feature "user signs up for account without avatar" do
   scenario "successfully" do
     visit "/users/sign_up"
 
-      fill_in "Username", with: "Sonic The Hedgehog"
-      fill_in "Email", with: "sonic@hedgehog.com"
-      fill_in "Password", with: "password"
-      fill_in "Password confirmation", with: "password"
+    fill_in "Username", with: "Sonic The Hedgehog"
+    fill_in "Email", with: "sonic@hedgehog.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
 
-      click_on "Sign up"
-      expect(page).to have_content "Welcome! You have signed up successfully."
-      within('div#user_avatar') do
-        expect(page).to have_xpath('//img')
+    click_on "Sign up"
+    expect(page).to have_content "Welcome! You have signed up successfully."
+    within('div#user_avatar') do
+      expect(page).to have_xpath('//img')
     end
   end
 
