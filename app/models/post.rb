1f502d9f9c9c0725c8a2370bf5ad91d98f816362
Michael Hebert <michaelhebert@Michaels-MacBook-Pro.local>
2016-04-13 13:38:57 -0400

    gave the dog a bone

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,15 +2,15 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :reviews
 
+  validates :webseries_name, presence: true
+  validates :link, presence: true
+  validates :description, presence: true, length: { minimum: 5 }
+
   def self.search(query)
-    where("webseries_name like ?", "%#{query}%") 
+    where("webseries_name like ?", "%#{query}%")
   end
 
   def name
     webseries_name
   end
-
-  validates :webseries_name, presence: true
-  validates :link, presence: true
-  validates :description, presence: true, length: { minimum: 5 }
 end
