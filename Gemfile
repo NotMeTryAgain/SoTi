874214eb13141d6a258d4e7121a20a72e138520a
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-04-14 12:15:27 -0400

    add faker, update seeds, add some front end

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 ruby '2.2.3'
 
+gem 'faker'
 gem 'selenium-webdriver'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 5.times do
-  FactoryGirl.create(:user)
+  FactoryGirl.create(:user, avatar: Faker::Avatar.image("my-own-slug", "50x50"))
 end
 
 FactoryGirl.create(:user, username: "pablo-admin", role: "admin")
 
 5.times do
-  FactoryGirl.create(:post, user: User.first)
-  FactoryGirl.create(:post_with_three_reviews)
+  FactoryGirl.create(:post, webseries_name: Faker::Hipster.sentence(3), user: User.first, description: Faker::Hipster.paragraph(2), link: "https://www.youtube.com/watch?v=YQHsXMglC9A", link_to_trailer: "https://www.youtube.com/watch?v=TkV-of_eN2w")
+  FactoryGirl.create(:post_with_three_reviews, webseries_name: Faker::Hipster.sentence(3))
 end
 
 Post.all.each do |post|
   20.times do
-    FactoryGirl.create(:review, post: post)
+    FactoryGirl.create(:review, post: post, body: Faker::Hipster.paragraph(2))
   end
 end
