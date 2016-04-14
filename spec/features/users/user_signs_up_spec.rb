f7d5696cdcac3287d68f3a3c7a77aa6d7663a696
Candace Hazlett <candace.hazlett@gmail.com>
2016-04-14 16:52:25 -0400

    Deleted test for default avatar

--- spec/features/users/user_signs_up_spec.rb
+++ spec/features/users/user_signs_up_spec.rb
@@ -20,22 +20,6 @@ feature "user signs up for account with avatar" do
   end
 end
 
-feature "user signs up for account without avatar" do
-  scenario "successfully" do
-    visit "/users/sign_up"
-
-    fill_in "Username", with: "Sonic The Hedgehog"
-    fill_in "Email", with: "sonic@hedgehog.com"
-    fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
-
-    click_on "Sign up"
-    expect(page).to have_content "Welcome! You have signed up successfully."
-    within('div#user_avatar') do
-      expect(page).to have_xpath('//img')
-    end
-  end
-
   scenario "passwords don't match" do
     visit "/users/sign_up"
 
