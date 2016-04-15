b3e773a4a8072ac9a1750c1646a4fbaeee547458
Michael Hebert <michaelhebert@Michaels-MacBook-Pro.local>
2016-04-15 12:22:33 -0400

    added styling to buttons, removed default avatar

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,10 +5,6 @@ class AvatarUploader < CarrierWave::Uploader::Base # :nodoc:
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # def default_url
-  #   'default_avatar.jpg'
-  # end
-
   version :large_avatar do
     process resize_to_fill: [150, 150]
   end
@@ -20,10 +16,6 @@ class AvatarUploader < CarrierWave::Uploader::Base # :nodoc:
   version :small_avatar do
     process resize_to_fill: [35, 35]
   end
-
-  # version :default_avatar do
-  #   process resize_to_fill: [35, 35]
-  # end
 
   def extension_white_list
     %w(jpg jpeg gif png)
